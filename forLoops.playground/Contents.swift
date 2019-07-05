6f27a9f92798145ae0ec781431550cd9cdb43071
sbrij001 <sbrij001@yahoo.com>
2019-07-05 17:01:29 -0400

    using for in loops with modulo operator

--- forLoops.playground/Contents.swift
+++ forLoops.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+
+//add together all of the numbers in an array
 var str = "Hello, playground"
 
 let arrayOfNumbers = [1,5,3,6]
@@ -8,3 +10,16 @@ for num in arrayOfNumbers {
     sum += num
 }
    print(sum)
+
+
+//print all the numbers from one to ten
+for number in 1...10 {
+    print(number)
+}
+
+
+// if you want to print out an interval of numbers you would use the where keyword.
+
+for number in 1..<10 where number % 2 == 0 {
+    print(number)
+}
